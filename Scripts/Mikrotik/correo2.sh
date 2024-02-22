7a8856f6f56fc48da8ba89dcc279c2dc40e649f8
José Alejandro Hernandez <36524233+carstair@users.noreply.github.com>
2024-02-22 10:21:52 -0600

    pruebas correo

---
+++ Scripts/Mikrotik/correo2.sh
@@ -0,0 +1,5 @@
+ tool/e-mail/send server=mail.yotateam.com.ni user=mk_notificaci
+on_clientes_corp_bk@yotateam.com.ni password=Yot@1234Z tls=starttls from=mk_notifi
+cacion_clientes_corp_bk@yotateam.com.ni port=25 to=barias@yotateam.com.ni cc=jaher
+nandez@yotateam.com.ni,jahernandez@yotateam.com.ni subject=Fibra_caida_cliente_con
+voy_of_hope body=Se_cae_enlace_principal_cliente_convoy_of_hope  
