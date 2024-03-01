18f00058097ecf23c40cacbb8e5bccf401624518
José Alejandro Hernandez <36524233+carstair@users.noreply.github.com>
2024-03-01 16:46:33 -0600

    push

--- Scripts/Mikrotik/correo2.sh
+++ Scripts/Mikrotik/correo2.sh
@@ -1,5 +1,1 @@
- tool/e-mail/send server=mail.yotateam.com.ni user=mk_notificaci
-on_clientes_corp_bk@yotateam.com.ni password=Yot@1234Z tls=starttls from=mk_notifi
-cacion_clientes_corp_bk@yotateam.com.ni port=25 to=barias@yotateam.com.ni cc=jaher
-nandez@yotateam.com.ni,jahernandez@yotateam.com.ni subject=Fibra_caida_cliente_con
-voy_of_hope body=Se_cae_enlace_principal_cliente_convoy_of_hope  
+tool/e-mail/send server=mail.yotateam.com.ni user=mk_notificaci on_clientes_corp_bk@yotateam.com.ni password=Yot@1234Z tls=starttls from=mk_notificacion_clientes_corp_bk@yotateam.com.ni port=25 to=barias@yotateam.com.ni cc=jahernandez@yotateam.com.ni,jahernandez@yotateam.com.ni subject=Fibra_caida_cliente_convoy_of_hope body=Se_cae_enlace_principal_cliente_convoy_of_hope  
